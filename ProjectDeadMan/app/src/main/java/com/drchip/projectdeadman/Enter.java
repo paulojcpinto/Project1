11cf81c0d213608425ff40ca89573f00c5ebac3f
DrChipMaster <andrecampos1998@gmail.com>
2019-11-16 20:44:41 +0000

    app

---
+++ ProjectDeadMan/app/src/main/java/com/drchip/projectdeadman/Enter.java
@@ -0,0 +1,51 @@
+package com.drchip.projectdeadman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class Enter extends AppCompatActivity {
+
+    Button btnLogin, btnRegister;
+    ImageView ivLogo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister= findViewById(R.id.btnRegister);
+
+        ivLogo = findViewById(R.id.ivLogo);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(Enter.this,Login.class));
+                Enter.this.finish();
+
+            }
+        });
+        ivLogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v,"Made By:  André Campos \r\n                           Paulo Pindo",Snackbar.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+}
---
+++ ProjectDeadMan/app/src/main/java/com/drchip/projectdeadman/Login.java
@@ -0,0 +1,49 @@
+package com.drchip.projectdeadman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+    AutoCompleteTextView etNick;
+    Button btnCancel, btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);   //todo send signal bluetooth to rasp or stm and see who is connected.
+
+        etNick =  findViewById(R.id.etNick);
+        btnCancel = findViewById(R.id.btnCancel);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        String [] names = {"James","John", "Jenny","Pila","Jenine","Jack","PauloGay"};  // lista de nomes para aparecer na ajuda
+
+        ArrayAdapter<String> adapter
+                = new ArrayAdapter<String>(this, R.layout.custom_design_autocomlete,names);
+
+        etNick.setThreshold(1);  //Numero de caraters que o utilizador percisa de por para começar a aparecer a funcao de autocomplete
+        etNick.setAdapter(adapter);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo implement bluetooth method
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(Login.this,Enter.class));
+                Login.this.finish();
+            }
+        });
+    }
+}
