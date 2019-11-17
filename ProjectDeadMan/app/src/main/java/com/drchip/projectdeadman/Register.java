6bec372dfde487c95ebdbcff8dfa6652d7cf0acc
DrChipMaster <andrecampos1998@gmail.com>
2019-11-17 22:15:38 +0000

    Fui eu que fiz

--- ProjectDeadMan/app/src/main/java/com/drchip/projectdeadman/Register.java
+++ ProjectDeadMan/app/src/main/java/com/drchip/projectdeadman/Register.java
@@ -1,55 +1,113 @@
 package com.drchip.projectdeadman;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.Calendar;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Register extends AppCompatActivity {
 
     EditText etDate, etMessage;
     DatePickerDialog datePickerDialog;
+    TextInputEditText etTeste;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
         etDate= findViewById(R.id.etDate);
-        
+        etMessage = findViewById(R.id.etMessage);
 
 
+        etMessage.setOnContextClickListener(new View.OnContextClickListener() {
+            @Override
+            public boolean onContextClick(View v) {
 
 
+                Toast.makeText(Register.this, "Primeiro", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
 
-      etDate.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-              final Calendar c = Calendar.getInstance();
-              int mYear = c.get(Calendar.YEAR); // current year
-              int mMonth = c.get(Calendar.MONTH); // current month
-              int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
-              // date picker dialog
-              datePickerDialog = new DatePickerDialog(Register.this,
-                      new DatePickerDialog.OnDateSetListener() {
 
-                          @Override
-                          public void onDateSet(DatePicker view, int year,
-                                                int monthOfYear, int dayOfMonth) {
-                              // set day of month , month and year value in the edit text
-                              etDate.setText(dayOfMonth + "/"
-                                      + (monthOfYear + 1) + "/" + year);
+        etMessage.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
 
-                          }
-                      }, mYear, mMonth, mDay);
-              datePickerDialog.show();
-          }
-      });
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    AlertDialog.Builder message = new AlertDialog.Builder(Register.this);
+                    LayoutInflater inflater = getLayoutInflater();
+                    final View dialogView = inflater.inflate(R.layout.message_dialog, null);
+                    final EditText etReleaseMessage = dialogView.findViewById(R.id.etReleaseMessage);
+
+                    message.setView(dialogView);
+                    message.setTitle("Message to Release");
+                    message.setMessage("Enter the message that you want to release here!!");
+
+                    message.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            etMessage.setText(etReleaseMessage.getText());
+                        }
+                    });
+                    message.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+                    message.show();
+                }
+
+                return false;
+            }
+        });
+
+        etDate.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    final Calendar c = Calendar.getInstance();
+                    int mYear = c.get(Calendar.YEAR); // current year
+                    int mMonth = c.get(Calendar.MONTH); // current month
+                    int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                    // date picker dialog
+                    datePickerDialog = new DatePickerDialog(Register.this,
+                            new DatePickerDialog.OnDateSetListener() {
+
+                                @Override
+                                public void onDateSet(DatePicker view, int year,
+                                                      int monthOfYear, int dayOfMonth) {
+                                    // set day of month , month and year value in the edit text
+                                    etDate.setText(dayOfMonth + "/"
+                                            + (monthOfYear + 1) + "/" + year);
+
+                                }
+                            }, mYear, mMonth, mDay);
+                    datePickerDialog.show();
+                }
+
+
+                return false;
+            }
+        });
+
+
     }
 }
